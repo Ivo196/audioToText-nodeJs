11885f9bb70479abb3e4c831f79ea622ccbaeb00
Ivo196 <ivo.tonioni@gmail.com>
2024-07-26 22:24:06 +0200

    fix error

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 import { AssemblyAI } from 'assemblyai';
+import { config } from "dotenv";
 
+config()
 
 const client = new AssemblyAI({
-    apiKey: process.env.APY_KEY
+    apiKey:process.env.APY_KEY
 })
 const audioUrl =
   'https://storage.googleapis.com/aai-web-samples/5_common_sports_injuries.mp3'
 
-const audioUrl2 =
-  './audio.mp3'
+const audioUrl2 = './audio.mp3'
 
 const params = {
-  audio: audioUrl,
+  audio: audioUrl2,
 }
 
 const run = async () => {
-   const trascript = await client.transcripts.transcribe(data)
+   const trascript = await client.transcripts.transcribe(params)
    console.log(trascript.text)
 }
 
-run
+run()
